79d7906dec2651d98836b8120a57badd75e05e0a
Bridget Mendoza <mendozabridget777@gmail.com>
2018-07-18 05:34:31 +0300

    Make failing test pass

--- api/v1/entries.py
+++ api/v1/entries.py
@@ -1,15 +1,15 @@
 from api.v1 import app
-from flask import jsonify
+from flask import jsonify, request, make_response
 
 # Dummy entries
 my_entries = [{
-    'entry_id': '1',
+    'entry_id': 1,
     'entry_date': '7 June 2018',
     'entry_time': '18 15',
     'title': 'Exhausted',
     'content': 'Today am spent!'
 }, {
-    'entry_id': '2',
+    'entry_id': 2,
     'entry_date': '8 June 2018',
     'entry_time': '18 15',
     'title': 'Excitement',
@@ -20,3 +20,15 @@ my_entries = [{
 @app.route('/api/v1/entries', methods=['GET'])
 def get_all_entries():
     return jsonify({'My entries': my_entries})
+
+
+@app.route('/api/v1/entries/<int:entry_id>', methods=['PUT'])
+def modify_entry(entry_id):
+    updating_data = request.get_json()
+    my_entry = [entry for entry in my_entries if entry['entry_id'] == entry_id]
+    if my_entry:
+        if 'title' in updating_data:
+            my_entry[0]['title'] = updating_data['title']
+        if 'content' in updating_data:
+            my_entry[0]['content'] = updating_data['content']
+        return make_response("Entry has been updated", 200)

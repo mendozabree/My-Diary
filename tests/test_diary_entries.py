380deaedd88596b1713a17f3db5fb0817f73fdbc
Bridget Mendoza <mendozabridget777@gmail.com>
2018-07-18 15:03:07 +0300

    Update test file to include test that checks for missing keys

--- tests/test_diary_entries.py
+++ tests/test_diary_entries.py
@@ -32,17 +32,32 @@ class DiaryEntryTestCase(unittest.TestCase):
                 'entry_title': 'My first Flask API',
                 'content': 'I am following some flask youtube videos from pretty printed. '
                            'I am going to work along with them then later create my own product.'
+            },
+            {
+                'entry_id': 3,
+                'entry_date': '20 June 2018',
+                'entry_title': 'Day Two Flask API',
+                'content': 'So, pretty printed is very nice channel, they explain pretty well. '
+                           'Yesterday, i created a simple todo list API using flask and SQLAlchemy'
             }
         ]
 
-    def test_API_can_not_make_an_entry_without_all_fields(self):
-        """Tests that the API will fail to create new entry if an field is missing"""
+    def test_API_can_not_make_an_entry_with_missing_values(self):
+        """Tests that the API will fail to create new entry if any value is blank"""
         testing_user = app.test_client(self)
         self.my_entries[0]['title'] = ""
         response = testing_user.post("/api/v1/entries", data=json.dumps(self.my_entries[0]),
                                      content_type="application/json")
         self.assertEqual(response.status_code, 200)
-        self.assertIn("All details are required", str(response.data))
+        self.assertIn("Enter value for title", str(response.data))
+
+    def test_API_can_not_make_an_entry_with_missing_key(self):
+        """Tests that the API will fail  to create a new entry if a key is missing"""
+        testing_user = app.test_client(self)
+        response = testing_user.post("/api/v1/entries", data=json.dumps(self.my_entries[2]),
+                                     content_type="application/json")
+        self.assertEqual(response.status_code, 200)
+        self.assertIn("Missing entry_time", str(response.data))
 
     def test_API_can_make_new_entry(self):
         """Tests that a new entry can be created"""

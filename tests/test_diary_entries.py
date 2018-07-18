16241b5c404ef484237c32c7d3a6c087810f4ed2
Bridget Mendoza <mendozabridget777@gmail.com>
2018-07-18 05:16:33 +0300

    Create a failing  test

--- tests/test_diary_entries.py
+++ tests/test_diary_entries.py
@@ -1,5 +1,6 @@
 import unittest
 from api.v1 import app
+import json
 
 
 class DiaryEntryTestCase(unittest.TestCase):
@@ -13,6 +14,20 @@ class DiaryEntryTestCase(unittest.TestCase):
         response = testing_user.get("/api/v1/entries", content_type="application/json")
         assert b'Exhausted' in response.data
         assert b'Excitement' in response.data
+
+    def test_modify_entry(self):
+        """Tests that a user can modify an entry"""
+        my_entry = {
+            'entry_id': 1,
+            'entry_date': '7 June 2018',
+            'entry_time': '18 15',
+            'title': 'Exhausted',
+            'content': 'Today am spent!'
+        }
+        testing_user = app.test_client(self)
+        my_entry['title'] = 'Tired!'
+        response = testing_user.put("/api/v1/entries/1", data=json.dumps(my_entry), content_type="application/json")
+        assert response.status_code == 200
 
 
 if __name__ == '__main__':

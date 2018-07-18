563c655bc8f8e6c98a79be97bb7383aadfcb39b4
Bridget Mendoza <mendozabridget777@gmail.com>
2018-07-18 13:26:15 +0300

    Update test file to include setUp function

--- tests/test_diary_entries.py
+++ tests/test_diary_entries.py
@@ -8,6 +8,26 @@ class DiaryEntryTestCase(unittest.TestCase):
         app.config['TESTING'] = True
         return app
 
+    def setUp(self):
+        self.my_entries = [
+            {
+                'entry_id': 1,
+                'entry_date': '18 June 2018',
+                'entry_time': '22 15',
+                'entry_title': 'Learning Flask',
+                'content': 'Flask is a micro-framework based on python. '
+                           'Flask is useful for designing APIs.'
+            },
+            {
+                'entry_id': 2,
+                'entry_date': '19 June 2018',
+                'entry_time': '22 15',
+                'entry_title': 'My first Flask API',
+                'content': 'I am following some flask youtube videos from pretty printed. '
+                           'I am going to work along with them then later create my own product.'
+            }
+        ]
+    
     def test_get_all_entries(self):
         """Tests that all entries can be retrieved"""
         testing_user = app.test_client(self)

640fd0aa9e8f2645a049cd56e162284eb7403afa
Bridget Mendoza <mendozabridget777@gmail.com>
2018-07-18 15:04:38 +0300

    Update class new_entry method to make past two tests pass

--- api/v1/my_classes.py
+++ api/v1/my_classes.py
@@ -4,9 +4,29 @@ class Entry:
         self.my_entries = []
 
     def create_new_entry(self, entry_data):
-        entry_data['entry_id'] = self.number_of_entries = self.number_of_entries + 1
-        self.my_entries.append(entry_data)
-        return self.my_entries
+        message = []
+        expected_key_list = ['entry_date', 'entry_time', 'title', 'content']
+        entry_data_key_list = [*entry_data.keys()]
+
+        odds = [x for x in expected_key_list if x not in entry_data_key_list]
+        if len(odds) != 0:
+            for key in odds:
+                error = 'Missing ' + key
+                message.append(error)
+
+        similar = [y for y in expected_key_list if y in entry_data_key_list]
+        for my_key in similar:
+            value = entry_data[my_key]
+            if value == '':
+                missing_value = 'Enter value for ' + my_key
+                message.append(missing_value)
+
+        if len(message) == 0:
+            entry_data['entry_id'] = self.number_of_entries = self.number_of_entries + 1
+            self.my_entries.append(entry_data)
+            return "Your memory has been saved"
+        else:
+            return message
 
     def get_specific_entry(self, entry_id):
         my_entry = [entry for entry in self.my_entries if entry['entry_id'] == entry_id]
